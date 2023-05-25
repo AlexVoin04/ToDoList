d89e67a3d2b75c03482232d1d9fae07615133588
alex <alexiv.04@mail.ru>
2023-05-25 09:16:56 +0200

    table data add and refr tasks

--- app/views.py
+++ app/views.py
@@ -12,6 +12,7 @@ RDB_HOST = 'localhost'
 RDB_PORT = 28015
 TODO_DB = 'todo'
 
+
 # db setup; only run once
 def dbSetup():
     connection = r.RethinkDB().connect(host=RDB_HOST, port=RDB_PORT)
@@ -23,7 +24,10 @@ def dbSetup():
         print('Database already exists.')
     finally:
         connection.close()
+
+
 dbSetup()
+
 
 # open connection before each request
 @app.before_request
@@ -33,6 +37,7 @@ def before_request():
     except RqlDriverError:
         abort(503, "Database connection could be established.")
 
+
 # close the connection after each request
 @app.teardown_request
 def teardown_request(exception):
@@ -41,11 +46,20 @@ def teardown_request(exception):
     except AttributeError:
         pass
 
-@app.route('/', methods = ['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-        form = TaskForm()
-        if form.validate_on_submit():
-                r.RethinkDB().table('todos').insert({"name":form.label.data}).run(g.rdb_conn)
-                return redirect(url_for('index'))
-        selection = list(r.RethinkDB().table('todos').run(g.rdb_conn))
-        return render_template('index.html', form = form, tasks = selection)
+    form = TaskForm()
+    if form.validate_on_submit():
+        count = r.RethinkDB().table('todos').count().run(g.rdb_conn)
+        if count == 0:
+            priority = 1
+        else:
+            priority_number = r.RethinkDB().table('todos').order_by(r.RethinkDB().desc('priority')).limit(1).pluck('priority').run(g.rdb_conn)
+            priority = (int(priority_number[0]['priority']) + 1)
+        r.RethinkDB().table('todos').insert(
+            {"name": form.label.data, "status_id": "5acb93df-e780-46fe-9973-e33acb36658f", "priority": priority}).run(g.rdb_conn)
+        return redirect(url_for('index'))
+    selection = list(r.RethinkDB().table('todos').eq_join('status_id', r.RethinkDB().table('todo_status')).without({"right": "id"}).zip().run(g.rdb_conn))
+    return render_template('index.html', form=form, tasks=selection)
+
