f1385d113ceec16eb6c21605592f487adf08610c
alex <alexiv.04@mail.ru>
2023-05-25 07:49:01 +0200

    test table task

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField
-#from wtforms.validators import Required
-from wtforms.validators import InputRequired
+# from wtforms.validators import Required
+from wtforms.validators import InputRequired, Length, Regexp
+
 
 class TaskForm(FlaskForm):
-	label = StringField('label', validators = [InputRequired()])
+    label = StringField('label', validators=[InputRequired(), Length(min=4), Regexp('^[\w,а-яА-Я\s]+$', message='Only English and Russian letters, digits, and commas are allowed')])
