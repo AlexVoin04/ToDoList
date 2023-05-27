7d229a6c53fd35bb2c2ca610c234c39999fdb1db
alex <alexiv.04@mail.ru>
2023-05-27 14:52:18 +0200

    add check all button

---
+++ app/static/js/script_for_checkbox.js
@@ -0,0 +1,9 @@
+// get all checkboxes
+ var checkboxes = document.getElementsByName('check_box');
+
+// add click event listener to the check-all button
+document.getElementById('check-all').addEventListener('click', function() {
+  checkboxes.forEach(function(checkbox) {
+    checkbox.checked = true;
+  });
+});
